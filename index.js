6aa88a803ebb59a897775a7d72e1503d05c38ae1
Florian LEFEBVRE <florian.lefebvre3184@orange.fr>
2021-03-25 16:10:51 +0100

    0.1.1

--- index.js
+++ index.js
@@ -1,31 +1,29 @@
-module.exports = function () {
-  return function ({ addUtilities, e, theme }) {
-    const colors = theme("colors");
-    const strokeColors = Object.keys(colors).reduce((acc, key) => {
-      if (typeof colors[key] === "string") {
-        return {
-          ...acc,
-          [`.stroke-${e(key)}`]: {
-            stroke: colors[key],
-          },
-        };
-      }
-
-      const variants = Object.keys(colors[key]);
-
+module.exports = function ({ addUtilities, e, theme }) {
+  const colors = theme("colors");
+  const strokeColors = Object.keys(colors).reduce((acc, key) => {
+    if (typeof colors[key] === "string") {
       return {
         ...acc,
-        ...variants.reduce(
-          (a, variant) => ({
-            ...a,
-            [`.stroke-${e(key)}-${variant}`]: {
-              stroke: colors[key][variant],
-            },
-          }),
-          {}
-        ),
+        [`.stroke-${e(key)}`]: {
+          stroke: colors[key],
+        },
       };
-    }, {});
-    addUtilities(strokeColors);
-  };
+    }
+
+    const variants = Object.keys(colors[key]);
+
+    return {
+      ...acc,
+      ...variants.reduce(
+        (a, variant) => ({
+          ...a,
+          [`.stroke-${e(key)}-${variant}`]: {
+            stroke: colors[key][variant],
+          },
+        }),
+        {}
+      ),
+    };
+  }, {});
+  addUtilities(strokeColors);
 };
